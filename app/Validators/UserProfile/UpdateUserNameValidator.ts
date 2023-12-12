bea7fba5ad079b70437789031cc7f83c7ddd5428
sceballos <dev.sj.ceballos@gmail.com>
2023-12-12 21:09:25 +0900

    added validator to for user name update route

---
+++ app/Validators/UserProfile/UpdateUserNameValidator.ts
@@ -0,0 +1,46 @@
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UpdateUserNameValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string([ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string([
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    name: schema.string({}, [rules.minLength(5), rules.maxLength(128)]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages: CustomMessages = {
+    'name.required': `Name is required.`,
+    'name.minLength': `Name should be at least 5 characters long.`,
+    'name.maxLength': `Name should be at most 128 characters long.`,
+  }
+}
