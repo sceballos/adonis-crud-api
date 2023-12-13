b855ec3988c5efc9b4cbbabf7018f0ade985352f
sceballos <dev.sj.ceballos@gmail.com>
2023-12-13 13:07:45 +0900

    added documentation to CryptoManager class

--- app/Managers/Auth/CryptoManager.ts
+++ app/Managers/Auth/CryptoManager.ts
@@ -1,6 +1,12 @@
 import crypto from 'crypto'
 
 export default class CryptoManager {
+  /**----------------------------------------------------------------------------------------
+   * public static GenerateHash
+   * @param input: string
+   * @description Generates and returns a SHA256 hash.
+   * @returns string
+   *----------------------------------------------------------------------------------------*/
   public static GenerateHash(input: string) {
     return crypto.createHash('sha256').update(input).digest('hex')
   }
